2bb2dc8776395980e55b1b065a3a12948eeae598
David <dwjmoore@gmail.com>
2022-11-27 16:00:20 -0700

    initial build out of the portfolio detail component

--- src/components/portfolio/PortfolioDetail.js
+++ src/components/portfolio/PortfolioDetail.js
@@ -1,7 +1,35 @@
-export default function PortfolioDetail(props) {
-    return (
-        <div>
-            <h2>Portfolio Detail for {props.match.params.slug}</h2>
-        </div>
-    );
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PortfolioDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.getPortfolioItem();
+    }
+
+    getPortfolioItem() {
+        axios
+            .get(
+                `https://jordan.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug
+                }`,
+                { withCredentials: true }
+            )
+            .then(response => {
+                console.log("res", response);
+            })
+            .catch(error => {
+                console.log("getportfolioitem error", error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+            </div>
+        );
+    }
 }
