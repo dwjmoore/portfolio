7fc631baa4d89170e5b81c85c181313e836e4ec7
David <dwjmoore@gmail.com>
2022-11-27 10:49:50 -0700

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import moment from "moment";
+import Navbar from "./components/navigation/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 import PortfolioContainer from "./components/portfolio/PortfolioContainer";
 
 export default function App() {
     return (
         <div className="app">
+            <Routes>
+                <Route path="/" element={<Navbar />} >
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                </Route>
+            </Routes>
+
             <h1>David W. J. Moore Portfolio</h1>
             <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <PortfolioContainer />
---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div>
+            <button>Home</button>
+            <button>About</button>
+            <button>Contact</button>
+        </div>
+    );
+}
