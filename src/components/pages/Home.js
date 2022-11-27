86963cd97025c0af576e0e1b8639c719e5c86e71
David <dwjmoore@gmail.com>
2022-11-27 14:44:16 -0700

    finalized styles for portfolio grid

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,6 @@ import PortfolioContainer from "../portfolio/PortfolioContainer";
 export default function Home() {
     return (
         <div>
-            <h2>Home</h2>
             <PortfolioContainer />
         </div>
     );
