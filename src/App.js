3eae2c2008913765ac62217684cdc9cf6b1f0fc1
David <dwjmoore@gmail.com>
2022-11-27 13:17:36 -0700

    add api call to portfolio container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import moment from "moment";
+import axios from "axios";
 
 import NavigationContainer from "./components/navigation/NavigationContainer";
 import Home from "./components/pages/Home";
@@ -15,7 +16,7 @@ export default class App extends Component {
             <div className="app">
                 <Router>
                     <div>
-                        <h1>David W. J. Moore Portfolio</h1>
+                        <h1>Jordan Hudgens Portfolio</h1>
                         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
                         <NavigationContainer />
 
@@ -23,7 +24,11 @@ export default class App extends Component {
                             <Route exact path="/" component={Home} />
                             <Route path="/about-me" component={About} />
                             <Route path="/contact" component={Contact} />
-                            <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+                            <Route
+                                exact
+                                path="/portfolio/:slug"
+                                component={PortfolioDetail}
+                            />
                             <Route component={NoMatch} />
                         </Switch>
                     </div>
